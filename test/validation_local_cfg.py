6e4bcd4d8ab7266ffa27a73f7821f5ec983faf20
Andrew Michael Kubik <sha1-11462ee980c99915eaf4bc9c9deba6cf31f9cb9d@cern.ch>
2008-07-18 20:52:00 +0000

    new python examples

--- test/validation_local_cfg.py
+++ test/validation_local_cfg.py
@@ -15,26 +15,51 @@ process.load("RecoLocalMuon.CSCRecHitD.cscRecHitD_cfi")
 
 process.load("RecoLocalMuon.CSCSegment.cscSegments_cfi")
 
+process.MuonNumberingInitialization = cms.ESProducer("MuonNumberingInitialization")
+
 process.maxEvents = cms.untracked.PSet(
     input = cms.untracked.int32(-1)
 )
-process.MuonNumberingInitialization = cms.ESProducer("MuonNumberingInitialization")
 
 process.source = cms.Source("DaqSource",
     readerPluginName = cms.untracked.string('CSCFileReader'),
     readerPset = cms.untracked.PSet(
-        RUI05 = cms.untracked.vstring('/uscms_data/d1/akub19/run39098/csc_00039098_EmuRUI05_Monitor_000.raw'),
-        RUI04 = cms.untracked.vstring('/uscms_data/d1/akub19/run39098/csc_00039098_EmuRUI04_Monitor_000.raw'),
-        RUI07 = cms.untracked.vstring('/uscms_data/d1/akub19/run39098/csc_00039098_EmuRUI07_Monitor_000.raw'),
-        RUI06 = cms.untracked.vstring('/uscms_data/d1/akub19/run39098/csc_00039098_EmuRUI06_Monitor_000.raw'),
         RUI01 = cms.untracked.vstring('/uscms_data/d1/akub19/run39098/csc_00039098_EmuRUI01_Monitor_000.raw'),
-        RUI10 = cms.untracked.vstring('/uscms_data/d1/akub19/run39098/csc_00039098_EmuRUI10_Monitor_000.raw'),
-        RUI03 = cms.untracked.vstring('/uscms_data/d1/akub19/run39098/csc_00039098_EmuRUI03_Monitor_000.raw'),
         RUI02 = cms.untracked.vstring('/uscms_data/d1/akub19/run39098/csc_00039098_EmuRUI02_Monitor_000.raw'),
-        RUI11 = cms.untracked.vstring('/uscms_data/d1/akub19/run39098/csc_00039098_EmuRUI11_Monitor_000.raw'),
-        RUI09 = cms.untracked.vstring('/uscms_data/d1/akub19/run39098/csc_00039098_EmuRUI09_Monitor_000.raw'),
+        RUI03 = cms.untracked.vstring('/uscms_data/d1/akub19/run39098/csc_00039098_EmuRUI03_Monitor_000.raw'),
+        RUI04 = cms.untracked.vstring('/uscms_data/d1/akub19/run39098/csc_00039098_EmuRUI04_Monitor_000.raw'),
+        RUI05 = cms.untracked.vstring('/uscms_data/d1/akub19/run39098/csc_00039098_EmuRUI05_Monitor_000.raw'),
+        RUI06 = cms.untracked.vstring('/uscms_data/d1/akub19/run39098/csc_00039098_EmuRUI06_Monitor_000.raw'),
+        RUI07 = cms.untracked.vstring('/uscms_data/d1/akub19/run39098/csc_00039098_EmuRUI07_Monitor_000.raw'),
         RUI08 = cms.untracked.vstring('/uscms_data/d1/akub19/run39098/csc_00039098_EmuRUI08_Monitor_000.raw'),
+        RUI09 = cms.untracked.vstring('/uscms_data/d1/akub19/run39098/csc_00039098_EmuRUI09_Monitor_000.raw'),
+        RUI10 = cms.untracked.vstring('/uscms_data/d1/akub19/run39098/csc_00039098_EmuRUI10_Monitor_000.raw'),
+        RUI11 = cms.untracked.vstring('/uscms_data/d1/akub19/run39098/csc_00039098_EmuRUI11_Monitor_000.raw'),
+        RUI12 = cms.untracked.vstring('/uscms_data/d1/akub19/run39098/csc_00039098_EmuRUI12_Monitor_000.raw'),
+        RUI13 = cms.untracked.vstring('/uscms_data/d1/akub19/run39098/csc_00039098_EmuRUI13_Monitor_000.raw'),
+        RUI14 = cms.untracked.vstring('/uscms_data/d1/akub19/run39098/csc_00039098_EmuRUI14_Monitor_000.raw'),
+        RUI15 = cms.untracked.vstring('/uscms_data/d1/akub19/run39098/csc_00039098_EmuRUI15_Monitor_000.raw'),
+        RUI16 = cms.untracked.vstring('/uscms_data/d1/akub19/run39098/csc_00039098_EmuRUI16_Monitor_000.raw'),
+        RUI17 = cms.untracked.vstring('/uscms_data/d1/akub19/run39098/csc_00039098_EmuRUI17_Monitor_000.raw'),
         RUI18 = cms.untracked.vstring('/uscms_data/d1/akub19/run39098/csc_00039098_EmuRUI18_Monitor_000.raw'),
+        RUI19 = cms.untracked.vstring('/uscms_data/d1/akub19/run39098/csc_00039098_EmuRUI19_Monitor_000.raw'),
+        RUI20 = cms.untracked.vstring('/uscms_data/d1/akub19/run39098/csc_00039098_EmuRUI20_Monitor_000.raw'),
+        RUI21 = cms.untracked.vstring('/uscms_data/d1/akub19/run39098/csc_00039098_EmuRUI21_Monitor_000.raw'),
+        RUI22 = cms.untracked.vstring('/uscms_data/d1/akub19/run39098/csc_00039098_EmuRUI22_Monitor_000.raw'),
+        RUI23 = cms.untracked.vstring('/uscms_data/d1/akub19/run39098/csc_00039098_EmuRUI23_Monitor_000.raw'),
+        RUI24 = cms.untracked.vstring('/uscms_data/d1/akub19/run39098/csc_00039098_EmuRUI24_Monitor_000.raw'),
+        RUI25 = cms.untracked.vstring('/uscms_data/d1/akub19/run39098/csc_00039098_EmuRUI25_Monitor_000.raw'),
+        RUI26 = cms.untracked.vstring('/uscms_data/d1/akub19/run39098/csc_00039098_EmuRUI26_Monitor_000.raw'),
+        RUI27 = cms.untracked.vstring('/uscms_data/d1/akub19/run39098/csc_00039098_EmuRUI27_Monitor_000.raw'),
+        RUI28 = cms.untracked.vstring('/uscms_data/d1/akub19/run39098/csc_00039098_EmuRUI28_Monitor_000.raw'),
+        RUI29 = cms.untracked.vstring('/uscms_data/d1/akub19/run39098/csc_00039098_EmuRUI29_Monitor_000.raw'),
+        RUI30 = cms.untracked.vstring('/uscms_data/d1/akub19/run39098/csc_00039098_EmuRUI30_Monitor_000.raw'),
+        RUI31 = cms.untracked.vstring('/uscms_data/d1/akub19/run39098/csc_00039098_EmuRUI31_Monitor_000.raw'),
+        RUI32 = cms.untracked.vstring('/uscms_data/d1/akub19/run39098/csc_00039098_EmuRUI32_Monitor_000.raw'),
+        RUI33 = cms.untracked.vstring('/uscms_data/d1/akub19/run39098/csc_00039098_EmuRUI33_Monitor_000.raw'),
+        RUI34 = cms.untracked.vstring('/uscms_data/d1/akub19/run39098/csc_00039098_EmuRUI34_Monitor_000.raw'),
+        RUI35 = cms.untracked.vstring('/uscms_data/d1/akub19/run39098/csc_00039098_EmuRUI35_Monitor_000.raw'),
+        RUI36 = cms.untracked.vstring('/uscms_data/d1/akub19/run39098/csc_00039098_EmuRUI36_Monitor_000.raw'),
         FED750 = cms.untracked.vstring('RUI00',
             'RUI01',
             'RUI02',
@@ -53,26 +78,34 @@ process.source = cms.Source("DaqSource",
             'RUI15',
             'RUI16',
             'RUI17',
-            'RUI18'),
-        RUI15 = cms.untracked.vstring('/uscms_data/d1/akub19/run39098/csc_00039098_EmuRUI15_Monitor_000.raw'),
-        RUI14 = cms.untracked.vstring('/uscms_data/d1/akub19/run39098/csc_00039098_EmuRUI14_Monitor_000.raw'),
-        RUI17 = cms.untracked.vstring('/uscms_data/d1/akub19/run39098/csc_00039098_EmuRUI17_Monitor_000.raw'),
-        RUI16 = cms.untracked.vstring('/uscms_data/d1/akub19/run39098/csc_00039098_EmuRUI16_Monitor_000.raw'),
-        RUI12 = cms.untracked.vstring('/uscms_data/d1/akub19/run39098/csc_00039098_EmuRUI12_Monitor_000.raw'),
-        RUI13 = cms.untracked.vstring('/uscms_data/d1/akub19/run39098/csc_00039098_EmuRUI13_Monitor_000.raw')
+            'RUI18',
+            'RUI19',
+            'RUI20',
+            'RUI21',
+            'RUI22',
+            'RUI23',
+            'RUI24',
+            'RUI25',
+            'RUI26',
+            'RUI27',
+            'RUI28',
+            'RUI29',
+            'RUI30',
+            'RUI31',
+            'RUI32',
+            'RUI33',
+            'RUI34',
+            'RUI35',
+            'RUI36')
     )
 )
 
 process.cscValidation = cms.EDFilter("CSCValidation",
+    rootFileName = cms.untracked.string('validationHists.root'),
     isSimulation = cms.untracked.bool(False),
-    makeComparisonPlots = cms.untracked.bool(False),
     writeTreeToFile = cms.untracked.bool(True),
-    refRootFile = cms.untracked.string('validationHists_local.root'),
-    rootFileName = cms.untracked.string('validationHists_run39098.root'),
     makePlots = cms.untracked.bool(False)
 )
 
 process.p = cms.Path(process.muonCSCDigis*process.csc2DRecHits*process.cscSegments*process.cscValidation)
-process.muonCSCDigis.ErrorMask = 0x0
-process.muonCSCDigis.ExaminerMask = 0x1FEBF3F6
 
---
+++ test/validation_sim_cfg.py
@@ -0,0 +1,37 @@
+import FWCore.ParameterSet.Config as cms
+
+process = cms.Process("TEST")
+process.load("Geometry.MuonCommonData.muonIdealGeometryXML_cfi")
+
+process.load("Geometry.CSCGeometry.cscGeometry_cfi")
+
+process.load("CalibMuon.Configuration.CSC_FakeDBConditions_cff")
+
+process.load("RecoLocalMuon.CSCRecHitD.cscRecHitD_cfi")
+process.csc2DRecHits.wireDigiTag = cms.InputTag("simMuonCSCDigis","MuonCSCWireDigi")
+process.csc2DRecHits.stripDigiTag = cms.InputTag("simMuonCSCDigis","MuonCSCStripDigi")
+
+
+process.load("RecoLocalMuon.CSCSegment.cscSegments_cfi")
+
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring('dcache:/pnfs/cms/WAX/11/store/relval/2008/7/13/RelVal-RelValSingleMuPt100-1215820444-IDEAL_V5-2nd/RelValSingleMuPt100/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/CMSSW_2_1_0_pre8-RelVal-1215820444-IDEAL_V5-2nd-IDEAL_V5-unmerged/0000/0E08D594-FA50-DD11-86D2-001617E30CD4.root',
+'dcache:/pnfs/cms/WAX/11/store/relval/2008/7/13/RelVal-RelValSingleMuPt100-1215820444-IDEAL_V5-2nd/RelValSingleMuPt100/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/CMSSW_2_1_0_pre8-RelVal-1215820444-IDEAL_V5-2nd-IDEAL_V5-unmerged/0000/527AF9C8-F550-DD11-8D8E-000423D99896.root',
+'dcache:/pnfs/cms/WAX/11/store/relval/2008/7/13/RelVal-RelValSingleMuPt100-1215820444-IDEAL_V5-2nd/RelValSingleMuPt100/GEN-SIM-DIGI-RAW-HLTDEBUG-RECO/CMSSW_2_1_0_pre8-RelVal-1215820444-IDEAL_V5-2nd-IDEAL_V5-unmerged/0000/F213F543-F550-DD11-9DE0-000423D6CA02.root')
+)
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(-1)
+)
+process.MuonNumberingInitialization = cms.ESProducer("MuonNumberingInitialization")
+
+process.cscValidation = cms.EDFilter("CSCValidation",
+    rootFileName = cms.untracked.string('validationHists.root'),
+    isSimulation = cms.untracked.bool(True),
+    writeTreeToFile = cms.untracked.bool(True),
+    makePlots = cms.untracked.bool(False)
+)
+
+
+process.p = cms.Path(process.csc2DRecHits*process.cscSegments*process.cscValidation)
+
